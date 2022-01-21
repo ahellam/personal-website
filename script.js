c40acee6d1adedf3244e629b663aa118801edd84
Aaron Hellam <aaronhellam@gmail.com>
2022-01-21 15:53:11 -0800

    stage 1 of personal website

---
+++ script.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement("h2");
+h2.textContent = "You should click this content added and edited by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  h2.addEventListener('click', clickAlert);
+
